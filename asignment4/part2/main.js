bd2b805508a2eb026292f48be7bf46235494624d
100947711 <kabin.kunwar@dcmail.ca>
2024-08-04 23:31:54 -0400

    image updated

--- asignment4/part2/main.js
+++ asignment4/part2/main.js
@@ -5,8 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic2.jpg' : 'Closeup of a human eye',
+  'pic3.jpg' : 'Rock that looks like a wave',
+  'pic4.jpg' : 'Purple and white pansies',
+  'pic1.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
 
