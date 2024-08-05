bcb2eed1ec1d935c5590418a0b63ab17ee8c6866
100947711 <kabin.kunwar@dcmail.ca>
2024-08-05 00:26:37 -0400

    code added

--- asignment4/part3/main.js
+++ asignment4/part3/main.js
@@ -28,4 +28,12 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+
+//making path and filling colour
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
   
